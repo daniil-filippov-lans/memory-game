a619c3a634e781e8962f9796477d0175b389e5a0
danil_lans <danimen0001@gmail.com>
2021-06-15 19:19:36 +0300

    мэйн

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<div id="root">
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
