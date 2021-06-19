3038d7b97151ee851e4c845bc2f93bd628af19e8
danil_lans <danimen0001@gmail.com>
2021-06-19 17:15:02 +0300

    перегнал на тс доску

--- components/Board.tsx
+++ components/Board.tsx
@@ -7,71 +7,83 @@ import * as L from '../lib/index';
 // let cell1 = ...
 // let board = [cell1, cell2, cell3 ...]
 
-export const getStatusAt = R.curry((i, board) => {
+export type Board = Cell.Cell[];
+
+export const getStatusAt = (i: number) => (board: Board) => {
 	return R.view(R.lensPath(`${i}.status`), board);
-});
+};
 
-export const setStatusAt = R.curry((i, status, board) => {
-	// 1. manual nesting
-	// 2. immer
-	// 3. lensimg
-	// return { ...board, [i]: { ...board[i], status } };
+export const setStatusAt =
+	(i: number) => (status: Cell.Status) => (board: Board) => {
+		// 1. manual nesting
+		// 2. immer
+		// 3. lensimg
+		// return { ...board, [i]: { ...board[i], status } };
 
-	return R.set(R.lensPath(`${i}.status`), status, board);
-});
+		return R.set(R.lensPath(`${i}.status`), status, board);
+	};
 
-export const setStatusesBy = R.curry((predFn, status, board) => {
-	return R.map(cell => (predFn(cell) ? { ...cell, status } : cell), board);
-});
+export const setStatusesBy =
+	(predFn: Cell.PredFn) => (status: Cell.Status) => (board: Board) => {
+		return R.map(
+			cell => (predFn(cell) ? { ...cell, status } : cell),
+			board
+		);
+	};
 
-export const getStatusesBy = R.curry((predFn, board) => {
+export const getStatusesBy = (predFn: Cell.PredFn) => (board: Board) => {
 	return R.chain(cell => (predFn(cell) ? [cell.status] : []), board);
-});
+};
 
-export const getSymbolsBy = R.curry((predFn, board) => {
+export const getSymbolsBy = (predFn: Cell.PredFn) => (board: Board) => {
 	return R.chain(cell => (predFn(cell) ? [cell.symbol] : []), board);
-});
+};
 
-export const canOpenAt = R.curry((i, board) => {
+export const canOpenAt = (i: number) => (board: Board) => {
 	return (
 		i < board.length &&
 		Cell.isClosed(board[i]) &&
-		getStatusesBy(Cell.isBlocking, board).length < 2
+		getStatusesBy(Cell.isBlocking)(board).length < 2
 	);
-});
+};
 
-export const areOpensEqual = board => {
-	const openSymbols = getSymbolsBy(Cell.isOpen, board);
+export const areOpensEqual = (board: Board) => {
+	const openSymbols = getSymbolsBy(Cell.isOpen)(board);
 	return openSymbols.length >= 2 && L.allEquals(openSymbols);
 };
 
-export const areOpensDifferent = board => {
-	const openSymbols = getSymbolsBy(Cell.isOpen, board);
+export const areOpensDifferent = (board: Board) => {
+	const openSymbols = getSymbolsBy(Cell.isOpen)(board);
 	return openSymbols.length >= 2 && !L.allEquals(openSymbols);
 };
 
 const charCodeA = 'A'.charCodeAt(0);
 
-export const makeRandom = (m, n) => {
+export const makeRandom = (m: number, n: number): Board => {
 	if ((m * n) / 2 > 26) throw new Error('too big');
 	if ((m * n) % 2) throw new Error('must be even');
 
 	return R.pipe(
 		() => R.range(0, (m * n) / 2), // ["A", "B", "C"]
-		R.map(i => String.fromCharCode(i + charCodeA)),
+		R.map((i: number) => String.fromCharCode(i + charCodeA)),
 		R.chain(x => [x, x]),
 		L.shuffle,
-		R.map(symbol => ({ symbol, status: Cell.Status.Closed }))
-	)();
+		R.map((symbol: string) => ({ symbol, status: Cell.Status.Closed }))
+	)() as Board;
 };
 
 // VIEW ======================================================
-export const BoardView = ({ board, onClickAt }) => {
+
+type BoardViewProps = {
+	board: Board;
+	onClickAt: (i: number) => void;
+};
+export const BoardView: React.FC<BoardViewProps> = ({ board, onClickAt }) => {
 	return (
 		<>
 			<div className="board">
 				{board.map((cell, i) => (
-					<Cell.View
+					<Cell.cellView
 						key={i}
 						cell={cell}
 						onClick={_ => onClickAt(i)}
@@ -93,7 +105,14 @@ export const BoardView = ({ board, onClickAt }) => {
 	);
 };
 
-export function ScreenView({ background, children }) {
+type ScreenViewProps = {
+	background: string;
+};
+
+export const ScreenView: React.FC<ScreenViewProps> = ({
+	background,
+	children,
+}) => {
 	return (
 		<>
 			<div className="screen">{children}</div>
@@ -118,4 +137,4 @@ export function ScreenView({ background, children }) {
 			`}</style>
 		</>
 	);
-}
+};
